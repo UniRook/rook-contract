8bc1ea56a87b9954941c8a7ba39c936d482c4cdb
jsgart <devteam@unirook.com>
2023-07-21 15:04:00 -0500

    fixed rovokeRole in recover, tested contract

--- src/Rook.sol
+++ src/Rook.sol
@@ -39,9 +39,13 @@ contract Rook is
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant LOCKER_ROLE = keccak256("LOCKER_ROLE");
+    bytes32 public constant PROFESSOR_ROLE = keccak256("PROFESSOR_ROLE");
+    bytes32 public constant REWARDER_ROLE = keccak256("REWARDER_ROLE");
 
     address public currentAdmin;
-    address public burnerContract; //The RKT (RookTranscript NFT ERC721 Contract)
+    address public lockerContract; //The RKT (RookTranscript NFT ERC721 Contract)
+    address public rewardsAccount; //The NFD (RookDegree NFT ERC721 Contract)
     bytes32 private hashedBackupAddress;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -50,7 +54,7 @@ contract Rook is
     }
 
     function initialize(address _backupAddress) public initializer {
-        __Context_init(); // Initialize the ContextUpgradeable
+        __Context_init();
         __ERC20_init("Rook", "ROOK");
         __ERC20Burnable_init();
         __ERC20Snapshot_init();
@@ -64,7 +68,10 @@ contract Rook is
         _grantRole(SNAPSHOT_ROLE, _msgSender());
         _grantRole(PAUSER_ROLE, _msgSender());
         _grantRole(MINTER_ROLE, _msgSender());
+        _grantRole(BURNER_ROLE, _msgSender());
         _grantRole(UPGRADER_ROLE, _msgSender());
+        _grantRole(PROFESSOR_ROLE, _msgSender());
+        _grantRole(REWARDER_ROLE, _msgSender());
 
         // Set the current admin to the msg.sender at initialization
         currentAdmin = _msgSender();
@@ -87,14 +94,20 @@ contract Rook is
         _grantRole(SNAPSHOT_ROLE, _msgSender());
         _grantRole(PAUSER_ROLE, _msgSender());
         _grantRole(MINTER_ROLE, _msgSender());
+        _grantRole(BURNER_ROLE, _msgSender());
         _grantRole(UPGRADER_ROLE, _msgSender());
+        _grantRole(PROFESSOR_ROLE, _msgSender());
+        _grantRole(REWARDER_ROLE, _msgSender());
 
         // Revoke all roles from the original admin
-        renounceRole(DEFAULT_ADMIN_ROLE, currentAdmin);
-        renounceRole(SNAPSHOT_ROLE, currentAdmin);
-        renounceRole(PAUSER_ROLE, currentAdmin);
-        renounceRole(MINTER_ROLE, currentAdmin);
-        renounceRole(UPGRADER_ROLE, currentAdmin);
+        _revokeRole(DEFAULT_ADMIN_ROLE, currentAdmin);
+        _revokeRole(SNAPSHOT_ROLE, currentAdmin);
+        _revokeRole(PAUSER_ROLE, currentAdmin);
+        _revokeRole(MINTER_ROLE, currentAdmin);
+        _revokeRole(BURNER_ROLE, currentAdmin);
+        _revokeRole(UPGRADER_ROLE, currentAdmin);
+        _revokeRole(PROFESSOR_ROLE, currentAdmin);
+        _revokeRole(REWARDER_ROLE, currentAdmin);
 
         // Set the previous backup address (function caller) as the current admin
         currentAdmin = _msgSender();
@@ -119,26 +132,83 @@ contract Rook is
         _mint(to, amount);
     }
 
-    function assignBurnerRole(
-        address _contract
-    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        //To be called once to implement NFT contract as the burner of ROOK tokens and recipricate Transcript,
-        //verify BURNER_ROLE is a contract, and that is can only be set once
-        require(
-            AddressUpgradeable.isContract(_contract),
-            "Assigned address must be a contract"
-        );
-
-        grantRole(BURNER_ROLE, _contract);
-        burnerContract = _contract;
-    }
-
-    //Function for NFT contract RKT (RookTranscript) to call to burn deposited tokens
+    // Function for  RKT (RookTranscript NFT contract) to call to burn deposited tokens
     function burnFrom(
         address account,
         uint256 amount
     ) public override onlyRole(BURNER_ROLE) {
         _burn(account, amount);
+    }
+
+    // Function call to set RKD (RookDegree NFD contract) to hold locked tokens for rewards
+    function setRewardsAccount(
+        address _contract
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            AddressUpgradeable.isContract(_contract),
+            "Assigned address must be a contract"
+        );
+        rewardsAccount = _contract;
+    }
+
+    // Function call to set RKD (RookDegree) as Rewarder Role
+    function assignRewarderRole(
+        address _contract
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            AddressUpgradeable.isContract(_contract),
+            "Assigned address must be a contract"
+        );
+
+        grantRole(REWARDER_ROLE, _contract);
+    }
+
+    function mintReward(
+        address to,
+        uint256 amount
+    ) public onlyRole(REWARDER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function rewardFromPool(
+        address to,
+        uint256 amount
+    ) public onlyRole(REWARDER_ROLE) {
+        require(
+            balanceOf(rewardsAccount) >= amount,
+            "Not enough tokens in rewards pool"
+        );
+        _transfer(rewardsAccount, to, amount);
+    }
+
+    //Grant RKT (RookTranscript) the locker role to lock tokens to NFD for rewards
+    function assignLockerRole(
+        address _contract
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            AddressUpgradeable.isContract(_contract),
+            "Assigned address must be a contract"
+        );
+
+        grantRole(LOCKER_ROLE, _contract);
+        lockerContract = _contract;
+    }
+
+    // Function for RKT (RookTranscript) to transfer deposited tokens to the RKD (RookDegree)
+    function lockTokens(address account, uint256 amount) public {
+        require(
+            hasRole(LOCKER_ROLE, _msgSender()),
+            "Must be RKT contract to lock tokens."
+        );
+
+        _transfer(account, rewardsAccount, amount);
+    }
+
+    //To be used to interact with RKT (RookTranscript contract)
+    function assignProfessorRole(
+        address professorAddress
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(PROFESSOR_ROLE, professorAddress);
     }
 
     function _beforeTokenTransfer(
